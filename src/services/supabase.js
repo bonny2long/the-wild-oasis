fdfe00761868169217ea32e2470292a61a7efe83
bonny2long <bmakaniankhondo@ivytech.edu>
2025-08-26 09:29:18 -0400

    Fixed  env variable issue

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 
 export const supabaseUrl = 'https://pamvuqdmccaveyvusawe.supabase.co'
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
